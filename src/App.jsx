15e1f5a6d5382f779c1905920c52e01d1a98522c
Marcela Almeida <m.almeida.mt@gmail.com>
2026-07-14 00:31:48 -0300

    feat: configuracao React Router e paginas iniciais

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
+import AppRoutes from "./routes/appRoutes";
 
- function App() {
-  return (
-    <h1>BarberFlow 💈</h1>
-  )
+function App() {
+  return <AppRoutes />;
 }
 
-export default App
+export default App;
