418f61ab6bf5a6d133f46ced2b630b9e4763619e
Marcela Almeida <m.almeida.mt@gmail.com>
2026-07-18 00:44:57 -0300

    feat: implementa estrutura inicial de autenticação e rotas protegidas

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -2,5 +2,5 @@ import {userContext} from 'react';
 import { AuthContext } from '../context/AuthContext';
 
 export function useAuth() {
-  return useContext(AuthContext);
+  return userContext(AuthContext);
 }
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,13 +1,18 @@
 import { BrowserRouter, Routes, Route  } from "react-router-dom";
 import Login from "../pages/Login";
 import Dashboard from "../pages/Dashboard";
+import PrivateRoute from "./PrivateRoute";
+
 
 function AppRoutes () {
     return (
         <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Login />}/> 
+            <Route path="/" element={<Login />}/>
+            <Route element={<PrivateRoute />}>
             <Route path="/Dashboard" element={<Dashboard />} />
+            </Route>
+            
         </Routes>
         </BrowserRouter>
     );
